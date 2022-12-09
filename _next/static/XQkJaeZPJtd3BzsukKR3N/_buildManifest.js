55d1b3f7c9abecdc2584eb3f9298a6490a093760
root <root@aleo.susanti.me>
2022-12-09 12:56:33 +0000

    Deploy gh-pages

---
+++ _next/static/XQkJaeZPJtd3BzsukKR3N/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fcss\u002F622f4846eff8cf1a.css","static\u002Fchunks\u002Fpages\u002Findex-7db622ad5957984d.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-75f8c62d1b846d4f.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
